f7bbb82a26c64442237f7112fe0e61089a9f76bd
Andrew Johnson <drewej@protonmail.com>
2018-05-02 18:16:24 -0400

    Completed report

--- analytic.py
+++ analytic.py
@@ -12,10 +12,13 @@ def analytic(mu, tvec, xvec, sigmat, invvel, gammaFunc):
     if not isinstance(tvec, numpy.ndarray):
         tvec = numpy.array(tvec, dtype=float)
     meshT, meshX = numpy.meshgrid(tvec, xvec)
+    out = numpy.empty_like(meshT)
+    if mu < 0:
+        out.fill(0)
+        return out
     meshXShift = meshX  * invvel / mu
     unitStep1 = numpy.where(meshXShift < meshT)
     unitStep0 = numpy.where(meshXShift >= meshT)
-    out = numpy.empty_like(meshT)
     out[unitStep0] = 0.0
     out[unitStep1] = (
         gammaFunc(mu, meshT[unitStep1] - meshXShift[unitStep1]) *
@@ -31,7 +34,7 @@ def gamma1(mu, t):
 #
 
 SIG_T = 7.45857E-01
-INVV = 1.08743E-07
+INVV = 1.08743E-07 * 1E4
 
 
 def pureAbsorberSoln(mu, tvec, xvec):
